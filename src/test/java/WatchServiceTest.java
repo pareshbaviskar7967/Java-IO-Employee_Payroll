011fb395bf726cd8f842033a2b84897790355b7f
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-02 05:58:45 +0530

    Updated Solution for UC-3

---
+++ src/test/java/WatchServiceTest.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class WatchServiceTest {
+    @Test
+    public void givenDirectoryWhenWatchedListsAllTheActivites() throws IOException {
+        Path path = Paths.get("C:/Users/pares/eclipse-workspace/Java-IO-Employee_Payroll/lib");
+        Files.list(path).filter(Files::isRegularFile).forEach(System.out::println);
+        new WatchServiceExample(path);
+    }
+}
